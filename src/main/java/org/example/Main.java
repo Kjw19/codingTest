1e90cd97476280b9b1442fa1c483f4b228334812
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-05-24 16:50:54 +0900

    백준 10699번

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String format = now.format(dateTimeFormatter);
+        System.out.println(format);
     }
 }
