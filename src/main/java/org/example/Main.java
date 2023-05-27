02496c91c1a4be7474ff3326c56bc32ee6957350
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-05-27 13:09:08 +0900

    백준 28062 준석이의 사탕 사기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,31 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        StringBuilder sb = new StringBuilder();//문자열 1개이상 있을 때 사용
 
-        for (char c : s.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                sb.append(Character.toLowerCase(c));
-            } else {
-                sb.append(Character.toUpperCase(c));
-            }
+        long N = sc.nextInt();
+        long sum = 0;
+        List<Long> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            long a = sc.nextInt();
+            list.add(a);
+            sum += a;
         }
-        System.out.println(sb.toString());
+
+
+        if (sum % 2 != 0) {
+            Optional<Long> min = list.stream().filter(a -> a % 2 != 0).min(Long::compareTo);
+            sum -= min.get();
+        }
+        System.out.println(sum);
     }
 }
