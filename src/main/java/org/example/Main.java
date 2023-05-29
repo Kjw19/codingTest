00b50f34e10bf81bdd397e51ff9a5d70c9e51d63
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-05-29 09:43:46 +0900

    자바 알고리즘 강의 4번 단어 뒤집기 StringBuilder().reverse().toString() 사용

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import java.io.IOException;
-
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -13,17 +11,11 @@ public class Main {
         String[] strs = new String[N]; //입력한 N에따라 배열크기 지정
         for (int i = 0; i < N; i++) {
             String str = sc.nextLine();
-            char[] words = str.toCharArray();
-            for (int k = 0; k < str.length()/2; k++) {//뒤집는 로직 절반만 반복돌림
-                char tmp = words[k];
-                words[k] = words[words.length - k -1];
-                words[words.length - k -1] = tmp;
-            }
-            strs[i] = new String(words);//뒤집은 단어 저장
+            str = new StringBuilder(str).reverse().toString(); // StringBuilder를 이용해 문자열 뒤집기
+            strs[i] = str; // 뒤집은 단어 저장
         }
         for (String str: strs) {
             System.out.println(str);
         }
     }
-
-    }
+}
