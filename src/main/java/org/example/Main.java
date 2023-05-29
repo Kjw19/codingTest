da3cf166dde1695f43e6c72e082dce3764444d35
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-05-29 09:32:30 +0900

    자바 알고리즘 강의 4번 단어 뒤집기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,16 +8,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] words = str.split(" "); //split사용하여 공백으로 단어 분할
-        String longWord = "";
-
-        for (String word : words) {
-            if (word.length() > longWord.length()) {
-                longWord = word;
+        int N = sc.nextInt();
+        sc.nextLine(); //뒤에 오는 줄바꿈 문자 제거
+        String[] strs = new String[N]; //입력한 N에따라 배열크기 지정
+        for (int i = 0; i < N; i++) {
+            String str = sc.nextLine();
+            char[] words = str.toCharArray();
+            for (int k = 0; k < str.length()/2; k++) {//뒤집는 로직 절반만 반복돌림
+                char tmp = words[k];
+                words[k] = words[words.length - k -1];
+                words[words.length - k -1] = tmp;
             }
+            strs[i] = new String(words);//뒤집은 단어 저장
         }
-        System.out.println(longWord);
+        for (String str: strs) {
+            System.out.println(str);
         }
+    }
 
     }
