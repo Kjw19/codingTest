a256414cee4704b8a74594c8d039d90adc8497a0
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-06-04 08:40:51 +0900

    자바 알고리즘 강의 8번 가장짦은 문자거리

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String a = sc.nextLine().toLowerCase();
-        StringBuilder sb = new StringBuilder();
+        String a = sc.next();
+        char b = sc.next().charAt(0);
+        int[] s = new int[a.length()];
+        int c = 200;
+
 
         for (int i = 0; i < a.length(); i++) {
-            if (Character.isLetter(a.charAt(i))) {
-                sb.append(a.charAt(i));
+            if (a.charAt(i) == b) {
+                c = 0;
+            }else {
+                c++;
             }
+            s[i] = c;
         }
-        String b = sb.toString();
-        String c = sb.reverse().toString();
-        if (c.equals(b)) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
+
+        c=200;
+        for (int j = a.length() -1; j >=0; j--) {
+            if (a.charAt(j) == b) {
+                c = 0;
+            } else {
+                c++;
+            }
+            s[j] = Math.min(s[j], c);
+        }
+
+        for (int k = 0; k < s.length; k++) {
+            System.out.print(s[k] + " ");
         }
     }
 }
