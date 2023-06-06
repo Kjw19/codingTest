13090d0397968abe47fa8c2a0e906149470c8948
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-06-06 11:40:12 +0900

    자바 알고리즘 강의 12번 암호 강의 개선버전 substring과 replaceAll사용

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,38 +11,14 @@ public class Main {
         sc.nextLine();
         String a = sc.nextLine();
 
-        List<Character> ab = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        List<String> list = new ArrayList<>();
-        int length = 7;
-
-        for (int k = 0; k < a.length(); k++) {
-            ab.add(a.charAt(k));
-        }
-
         for (int i = 0; i < count; i++) {
-            for (int j = 0; j < length && !ab.isEmpty(); j++) {
-                sb.append(ab.get(0));
-                ab.remove(0);
-            }
-            list.add(sb.toString());
-            sb = new StringBuilder();
-        }
-        for (int m = 0; m < list.size(); m++) {
-            StringBuilder newSb = new StringBuilder();
-            String chunk = list.get(m);
-            for (int n = 0; n < chunk.length(); n++) {
-                if (chunk.charAt(n) == '#') {
-                    newSb.append(1);
-                } else if (chunk.charAt(n) == '*'){
-                    newSb.append(0);
-                }
-            }
-            int parseInt = Integer.parseInt(newSb.toString(), 2);
-            char anInt = (char) parseInt;
-            System.out.print(anInt);
-        }
+            String b = a.substring(i * 7, (i + 1) * 7)
+                    .replaceAll("#", "1")
+                    .replaceAll("\\*", "0");
 
+            int asc = Integer.parseInt(b, 2);
+            System.out.println((char) asc);
+        }
 
     }
 }
