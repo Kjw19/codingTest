aa953ed891fc8c0363e6ddd83cfeee16c333cb9f
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-06-06 11:32:05 +0900

    자바 알고리즘 강의 12번 암호

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,47 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int count = sc.nextInt();
+        sc.nextLine();
         String a = sc.nextLine();
-        int count =1;
+
+        List<Character> ab = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < a.length(); i++) {
-            if (a.charAt(i - 1) == a.charAt(i)) {
-                count++;
-            } else {
-                sb.append(a.charAt(i - 1));
-                if (count != 1) {
-                    sb.append(count);
-                }
-                count=1;
-            }
-        }
-        sb.append(a.charAt(a.length() - 1));
-        if (count != 1) {
-            sb.append(count);
+        List<String> list = new ArrayList<>();
+        int length = 7;
+
+        for (int k = 0; k < a.length(); k++) {
+            ab.add(a.charAt(k));
         }
 
-        String b = sb.toString();
-        System.out.println(b);
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < length && !ab.isEmpty(); j++) {
+                sb.append(ab.get(0));
+                ab.remove(0);
+            }
+            list.add(sb.toString());
+            sb = new StringBuilder();
+        }
+        for (int m = 0; m < list.size(); m++) {
+            StringBuilder newSb = new StringBuilder();
+            String chunk = list.get(m);
+            for (int n = 0; n < chunk.length(); n++) {
+                if (chunk.charAt(n) == '#') {
+                    newSb.append(1);
+                } else if (chunk.charAt(n) == '*'){
+                    newSb.append(0);
+                }
+            }
+            int parseInt = Integer.parseInt(newSb.toString(), 2);
+            char anInt = (char) parseInt;
+            System.out.print(anInt);
+        }
 
 
     }
