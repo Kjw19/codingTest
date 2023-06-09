67cd34488454289780591527698dbb1d5b908943
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-06-09 09:56:45 +0900

    자바 알고리즘 강의 3번 가위바위보

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,43 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] list = new int[N];
-        List<Integer> list2 = new ArrayList<>();
-
+        int[] A = new int[N];
+        int[] B = new int[N];
+        int sis = 1;
+        int rock = 2;
+        int paper = 3;
 
         for (int i = 0; i < N; i++) {
-            int a = sc.nextInt();
-            list[i] = a;
+            A[i] = sc.nextInt();
         }
-        list2.add(list[0]);
-        for (int j = 1; j < list.length; j++) {
-            if (list[j - 1] < list[j]) {
-                list2.add(list[j]);
+        for (int i = 0; i < N; i++) {
+            B[i] = sc.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            if (A[i] == B[i]) {
+                System.out.println("D");
+            } else if (A[i] == sis && B[i] == rock) {
+                System.out.println("B");
+            } else if (A[i] == sis && B[i] == paper) {
+                System.out.println("A");
+            } else if (A[i] == rock && B[i] == sis) {
+                System.out.println("A");
+            } else if (A[i] == rock && B[i] == paper) {
+                System.out.println("B");
+            } else if (A[i] == paper && B[i] == sis) {
+                System.out.println("B");
+            } else if (A[i] == paper && B[i] == rock) {
+                System.out.println("A");
             }
-        }
-        for (Integer integer : list2) {
-            System.out.print(integer + " ");
         }
         }
 
-    }
+        }
 
