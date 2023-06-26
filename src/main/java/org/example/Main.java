7d4ee92931ad7098f4a7dba3fdf6dfdb916c94dd
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-06-26 15:13:42 +0900

    자바 알고리즘 강의 02-09 격자판 최대합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,25 +8,59 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
-        int count =1;
+        int[][] list = new int[n][n];
+        List<Integer> max = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            list.add(sc.nextInt());
-        }
-        searchRank(list, count);
-    }
-
-    private static void searchRank(List<Integer> list, int count) {
-        for (Integer integer : list) {
-            for (int i = 0; i < list.size(); i++) {
-                if (integer < list.get(i)) {
-                    count++;
-                }
+            for (int j = 0; j < n; j++) {
+                list[i][j] += sc.nextInt();
             }
-            System.out.print(count + " ");
-            count = 1;
         }
+
+        searchMax(n, list, max);
     }
 
-}
+    private static void searchMax(int n, int[][] list, List<Integer> max) {
+        columnSum(n, list, max);
+        rowSum(n, list, max);
+        diagonal(n, list, max);
+        reverseDiagonal(n, list, max);
+
+        Optional<Integer> optionalMax = max.stream().max(Integer::compareTo);
+        Integer maxValue = optionalMax.get();
+        System.out.println(maxValue);
+    }
+
+    private static void columnSum(int n, int[][] list, List<Integer> max) {
+        for (int i = 0; i < n; i++) {
+            int total = 0;
+            for (int j = 0; j < n; j++) {
+                total += list[i][j];
+            }
+            max.add(total);
+        }
+    }
+    private static void rowSum(int n, int[][] list, List<Integer> max) {
+        for (int i = 0; i < n; i++) {
+            int total = 0;
+            for (int j = 0; j < n; j++) {
+                total += list[j][i];
+            }
+            max.add(total);
+        }
+    }
+    private static void diagonal(int n, int[][] list, List<Integer> max) {
+            int total = 0;
+            for (int j = 0; j < n; j++) {
+                total += list[j][j];
+            }
+            max.add(total);
+        }
+    private static void reverseDiagonal(int n, int[][] list, List<Integer> max) {
+            int total = 0;
+            for (int j = 0; j < n; j++) {
+                total += list[j][j];
+            }
+            max.add(total);
+        }
+    }
