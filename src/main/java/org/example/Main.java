8dd33936b8701810304b761bfa48034863018892
김재우 <ksg3477@naver.com>
2023-08-21 16:58:48 +0900

    연속 부분수열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,25 +8,55 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int K = sc.nextInt();
-        int max=0;
-
-        List<Integer> price = new ArrayList<>();
+        int M = sc.nextInt();
+        int[] numbers = new int[N];
 
         for (int i = 0; i < N; i++) {
-            price.add(sc.nextInt());
-        }
-        for (int h = 0; h < K; h++) {
-            max += price.get(h);
+            numbers[i] = sc.nextInt();
         }
 
+        int count=0;
+        int start = 0;
+        int end =0;
+        int sum = 0;
 
-        int element = max;
-        for (int j = K; j < N; j++) {
-            element += price.get(j) - price.get(j - K);
-            max = Math.max(max, element);
+        while (true) {
+            if (sum >= M) {
+                sum -= numbers[start++];
+            } else if (end == N) {
+                break;
+            } else {
+                sum += numbers[end++];
+            }
+            if (sum == M) {
+                count++;
+            }
         }
-        System.out.println(max);
+        System.out.println(count);
+
+//        int sum =0;
+//        int cnt = 0;
+//        int index = 0;
+//        List<Integer> list = new ArrayList<>();
+//
+//        for (int i = 0; i < N; i++) {
+//            list.add(sc.nextInt());
+//        }
+//
+//        for (int k = index; k < N; k++) {
+//            sum += list.get(k);
+//            if (sum == M) {
+//                cnt++;
+//                index++;
+//                k = index -1;
+//                sum = 0;
+//            } else if (sum > M) {
+//                index++;
+//                k = index -1;
+//                sum = 0;
+//            }
+//        }
+//        System.out.println(cnt);
     }
 }
 
